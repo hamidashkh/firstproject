6039a3ea92876858b818bb7ecfa1a4ce16992a02
hamidashkh <hamidashkh@code.edu.az>
2022-08-31 23:33:49 +0400

    Add project files.

---
+++ firstrepo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace firstrepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 7;
+            byte b = 8;
+            
+           
+        }
+    }
+}
